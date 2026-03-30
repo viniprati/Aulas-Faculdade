aa8bcfe9e4d8b349985097e834b2d1554723f1cf
VINICIUS PRATI MACHADO <162629373+viniprati@users.noreply.github.com>
2026-03-30 15:41:13 -0300

    Atividade 1

---
+++ Desenvolvimento Fron-end/Faixa Etária/script.js
@@ -0,0 +1,16 @@
+function calcular() {
+    let jovens = Number(document.getElementById("jovens").value)
+    let adultos = Number(document.getElementById("adultos").value)
+    let idosos = Number(document.getElementById("idosos").value)
+
+    let total = jovens + adultos + idosos
+
+    let porcentagemJovens = (jovens * 100) / total
+    let porcentagemAdultos = (adultos * 100) / total
+    let porcentagemIdosos = (idosos * 100) / total
+
+    document.getElementById("resultado").innerHTML =
+        "Jovens: " + porcentagemJovens.toFixed(2) + "%<br>" +
+        "Adultos: " + porcentagemAdultos.toFixed(2) + "%<br>" +
+        "Idosos: " + porcentagemIdosos.toFixed(2) + "%"
+}
